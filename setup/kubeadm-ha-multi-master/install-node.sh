78f1ef62ebfe474f63269819b5b55b4bfc7d9ee3
root <root@kmaster1.example.com>
2020-12-24 19:16:39 +0000

    Multi-master

---
+++ setup/kubeadm-ha-multi-master/install-node.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+ufw disable
+swapoff -a; sed -i '/swap/d' /etc/fstab
+cat >>/etc/sysctl.d/kubernetes.conf<<EOF
+net.bridge.bridge-nf-call-ip6tables = 1
+net.bridge.bridge-nf-call-iptables = 1
+EOF
+sysctl --system
+
+echo "installing docker"
+apt-get update
+apt-get install -y \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+add-apt-repository \
+   "deb https://download.docker.com/linux/$(. /etc/os-release; echo "$ID") \
+   $(lsb_release -cs) \
+   stable"
+apt-get update && apt-get install -y docker-ce=5:19.03.10~3-0~ubuntu-focal containerd.io
+
+echo "installing kubeadm and kubectl"
+apt-get update && apt-get install -y apt-transport-https
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
+cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
+deb http://apt.kubernetes.io/ kubernetes-xenial main
+EOF
+apt-get update
+apt-get install -y kubeadm=1.19.2-00 kubelet=1.19.2-00 kubectl=1.19.2-00
